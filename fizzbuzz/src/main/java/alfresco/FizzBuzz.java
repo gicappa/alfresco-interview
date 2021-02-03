c30661dcef617af276e5a4525181a2fd3d54c202
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-03 23:03:57 +0100

    It generates 'fizz' when the input is 3

--- fizzbuzz/src/main/java/alfresco/FizzBuzz.java
+++ fizzbuzz/src/main/java/alfresco/FizzBuzz.java
@@ -2,6 +2,10 @@ package alfresco;
 
 public class FizzBuzz {
     public String generate(int number) {
+        if (number == 3) {
+            return "fizz";
+        }
+
         return String.valueOf(number);
     }
 }
--- fizzbuzz/src/test/java/alfresco/FizzBuzzTest.java
+++ fizzbuzz/src/test/java/alfresco/FizzBuzzTest.java
@@ -1,5 +1,6 @@
 package alfresco;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +8,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class FizzBuzzTest {
 
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void before() {
+        fizzBuzz = new FizzBuzz();
+    }
+
     @Test
-    @DisplayName("Given a number in input generates a number in the output string")
+    @DisplayName("Given a number in input it returns the same number as a string")
     void it_prints_a_number() {
-        var fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.generate(1)).isEqualTo("1");
+    }
+
+    @Test
+    @DisplayName("Given 3 in input generates a the string 'fizz'")
+    void it_prints_fizz_when_the_number_is_three() {
+        assertThat(fizzBuzz.generate(3)).isEqualTo("fizz");
     }
 }
