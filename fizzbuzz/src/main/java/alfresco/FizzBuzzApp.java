c92a224127e24b32534748a8fdd5c2091a1085b8
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-05 23:44:47 +0100

    Improving readability and documenting the code

--- fizzbuzz/src/main/java/alfresco/FizzBuzzApp.java
+++ fizzbuzz/src/main/java/alfresco/FizzBuzzApp.java
@@ -2,27 +2,46 @@ package alfresco;
 
 import java.util.stream.IntStream;
 
+import static java.lang.Integer.parseInt;
 import static java.util.stream.Collectors.joining;
 
 /**
  * The fizz buzz app prints out a series of numbers
  */
 public class FizzBuzzApp {
+
     private final FizzBuzz fizzBuzz;
 
     public static void main(String[] args) {
         var app = new FizzBuzzApp(new FizzBuzz());
-        app.generate(Integer.valueOf(args[0]));
+        app.generate(parseInt(args[0]));
     }
 
+    /**
+     * @param fizzBuzz collaborator that encodes numbers
+     */
     public FizzBuzzApp(FizzBuzz fizzBuzz) {
         this.fizzBuzz = fizzBuzz;
     }
 
-    public void generate(Integer rangeEnd) {
-        System.out.println(
-                IntStream.rangeClosed(1, rangeEnd)
-                        .mapToObj(fizzBuzz::generate)
-                        .collect(joining(" ")));
+    /**
+     * Print all the fizz buzz values in a range
+     *
+     * @param rangeEnd end number of the range
+     */
+    public void generate(int rangeEnd) {
+        System.out.println(generatesFizzBuzzUpTo(rangeEnd));
+    }
+
+    /**
+     * Generates all fizz buzz values up to a range limit
+     *
+     * @param rangeEnd end number of the range
+     * @return a string containing the generated values
+     */
+    private String generatesFizzBuzzUpTo(int rangeEnd) {
+        return IntStream.rangeClosed(1, rangeEnd)
+                .mapToObj(fizzBuzz::generate)
+                .collect(joining(" "));
     }
 }
