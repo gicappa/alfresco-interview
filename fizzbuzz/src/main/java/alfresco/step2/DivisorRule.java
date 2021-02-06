44a5d05b865ef109fa1e0d13f6fd4dbc5245ebef
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-06 19:05:36 +0100

    Extracted the divisor rule in a new file

---
+++ fizzbuzz/src/main/java/alfresco/step2/DivisorRule.java
@@ -0,0 +1,46 @@
+package alfresco.step2;
+
+/**
+ * The rule is applied when the number passed
+ * is a divisor of the number.
+ *
+ * If the rule is matched the result is returned
+ * with an additional special word in it.
+ */
+class DivisorRule {
+
+    private final int divisor;
+    private final String word;
+
+    DivisorRule(int divisor, String word) {
+        this.divisor = divisor;
+        this.word = word;
+    }
+
+    /**
+     * Apply the rule to the number
+     *
+     * @param number to be parsed
+     * @param result the result coming from the previous rules
+     * @return the result with the applied rules
+     */
+    String apply(int number, String result) {
+        return result + translate(number);
+    }
+
+    private String translate(int number) {
+        if (multipleOf(number, divisor))
+            return word;
+
+        return "";
+    }
+
+    /**
+     * @param number  the number to be checked
+     * @param divider the divider to be checked
+     * @return true if number is a multiple of divider
+     */
+    private boolean multipleOf(int number, int divider) {
+        return number % divider == 0;
+    }
+}
--- fizzbuzz/src/main/java/alfresco/step2/FizzBuzz.java
+++ fizzbuzz/src/main/java/alfresco/step2/FizzBuzz.java
@@ -17,8 +17,8 @@ public class FizzBuzz {
     public String generate(int number) {
         String result = "";
 
-        result = new Rule(3, "fizz").apply(number, result);
-        result = new Rule(5, "buzz").apply(number, result);
+        result = new DivisorRule(3, "fizz").apply(number, result);
+        result = new DivisorRule(5, "buzz").apply(number, result);
 
         if (String.valueOf(number).contains("3")) {
             result = "alfresco";
@@ -31,41 +31,4 @@ public class FizzBuzz {
         return result;
     }
 
-    static class Rule {
-        private final int divisor;
-        private final String word;
-
-        Rule(int divisor, String word) {
-            this.divisor = divisor;
-            this.word = word;
-        }
-
-        /**
-         * Apply the rule to the number
-         *
-         * @param number to be parsed
-         * @param result the result coming from the previous rules
-         * @return the result with the applied rules
-         */
-        String apply(int number, String result) {
-            return result + translate(number);
-        }
-
-        private String translate(int number) {
-            if (multipleOf(number, divisor))
-                return word;
-
-            return "";
-        }
-
-        /**
-         * @param number  the number to be checked
-         * @param divider the divider to be checked
-         * @return true if number is a multiple of divider
-         */
-        private boolean multipleOf(int number, int divider) {
-            return number % divider == 0;
-        }
-
-    }
 }
