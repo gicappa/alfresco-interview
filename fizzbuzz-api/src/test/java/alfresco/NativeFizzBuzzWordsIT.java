1301a60307046777450ad31c7e893c9f5664934b
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-09 00:15:27 +0100

    adding an API layer implemented by quarkus.io

---
+++ fizzbuzz-api/src/test/java/alfresco/NativeFizzBuzzWordsIT.java
@@ -0,0 +1,9 @@
+package alfresco;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeFizzBuzzWordsIT extends FizzBuzzWordsTest {
+
+    // Execute the same tests but in native mode.
+}
