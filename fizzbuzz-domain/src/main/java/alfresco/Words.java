11b06a1c54f36809bf4d8d789ce0eda7a494964a
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-11 19:33:09 +0100

    Documenting the code

--- fizzbuzz-domain/src/main/java/alfresco/Words.java
+++ fizzbuzz-domain/src/main/java/alfresco/Words.java
@@ -14,27 +14,60 @@ public class Words {
 
     private final List<String> words;
 
+    /**
+     * Default constructor of a Words instance
+     * with an empty list of values
+     */
     public Words() {
         this(new ArrayList<>());
     }
 
+    /**
+     * Constructor to build a Words instance with
+     * a list of values passed to the constructor
+     *
+     * @param words the list of values to build the object
+     */
     public Words(List<String> words) {
         this.words = words;
     }
 
+    /**
+     * @return the list of values to be added
+     */
     public List<String> getWords() {
         return words;
     }
 
-    public void add(String string) {
-        words.add(string);
+    /**
+     * Add a word value to the Words object instance
+     *
+     * @param value the value to be added
+     */
+    public void add(String value) {
+        words.add(value);
     }
 
+    /**
+     * Adds the list of the generated words to the one in
+     * input
+     *
+     * @param words the external object that contains the
+     *              words to be added
+     * @return this instance with the word of the other
+     * object added
+     */
     public Words addAll(Words words) {
         this.words.addAll(words.getWords());
         return this;
     }
 
+    /**
+     * Helper to create a words
+     *
+     * @param array vararg to create a Words object
+     * @return an instance of the class Words
+     */
     public static Words of(String... array) {
         return new Words(Arrays.stream(array).collect(toList()));
     }
