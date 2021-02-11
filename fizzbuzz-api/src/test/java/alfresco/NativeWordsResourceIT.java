51a655f5e35d82cb0476e479d3f2db147157f9d7
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-11 14:03:35 +0100

    Renamed the FizzBuzzWords to WordsResource (also for tests)

--- fizzbuzz-api/src/test/java/alfresco/NativeWordsResourceIT.java
+++ fizzbuzz-api/src/test/java/alfresco/NativeWordsResourceIT.java
@@ -3,7 +3,7 @@ package alfresco;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeFizzBuzzWordsIT extends FizzBuzzWordsTest {
+public class NativeWordsResourceIT extends WordsResourceTest {
 
     // Execute the same tests but in native mode.
 }
