bd0d408e0886b3fe14db6252e69c150f67eee43b
Gian Carlo Pace <giancarlo.pace@gmail.com>
2021-02-14 17:50:18 +0100

    Configuring and documenting swagger-ui

--- fizzbuzz-api/src/main/java/alfresco/words/WordsResource.java
+++ fizzbuzz-api/src/main/java/alfresco/words/WordsResource.java
@@ -3,6 +3,13 @@ package alfresco.words;
 import alfresco.AppContext;
 import alfresco.ErrorReponse;
 import alfresco.WordGeneratorService;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameters;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -22,6 +29,27 @@ public class WordsResource {
 
     @GET
     @Path("words")
+    @Operation(
+        summary = "Get a list of fizzbuzz words",
+        description = "FizzBuzz maps a number positive number into a string according to" +
+            "a set of rule. The number can be translated in the same number in string format" +
+            "or in a string 'fizz' or 'buzz' or 'fizzbuzz' or 'alfresco'")
+    @Parameters(value = @Parameter(name = "limit",
+        description = "it is the upper bound of the fizzbuzz words generation"))
+    @APIResponses(value = {
+        @APIResponse(
+            responseCode = "200",
+            description = "It returns the list of fizzbuzz words generated translating" +
+                "number from 1 to the 'limit' parameter",
+            content = @Content(schema = @Schema(implementation = WordsResponse.class))),
+        @APIResponse(
+            responseCode = "400",
+            description = "The 'limit' params is not in a number format",
+            content = @Content(schema = @Schema(implementation = ErrorReponse.class))),
+        @APIResponse(
+            responseCode = "500",
+            description = "Internal Error generate by an unhandled exception",
+            content = @Content(schema = @Schema(implementation = ErrorReponse.class)))})
     public Response words(
         @DefaultValue("20")
         @QueryParam("limit")
